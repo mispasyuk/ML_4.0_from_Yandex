95b714c42cd7782a5351e01718f480952d62998f
mispasyuk <157352790+mispasyuk@users.noreply.github.com>
2025-09-29 13:15:44 +0300

    added first homework

---
+++ hw_1/template_p01.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+def softmax(vector):
+    '''
+    vector: np.array of shape (n, m)
+    
+    return: np.array of shape (n, m)
+        Matrix where softmax is computed for every row independently
+    '''
+    nice_vector = vector - vector.max()
+    exp_vector = np.exp(nice_vector)
+    exp_denominator = np.sum(exp_vector, axis=1)[:, np.newaxis]
+    softmax_ = exp_vector / exp_denominator
+    return softmax_
+
+def multiplicative_attention(decoder_hidden_state, encoder_hidden_states, W_mult):
+    '''
+    decoder_hidden_state: np.array of shape (n_features_dec, 1)
+    encoder_hidden_states: np.array of shape (n_features_enc, n_states)
+    W_mult: np.array of shape (n_features_dec, n_features_enc)
+    
+    return: np.array of shape (n_features_enc, 1)
+        Final attention vector
+    '''
+    # your code here
+    s = decoder_hidden_state
+    w = W_mult
+    h = encoder_hidden_states
+    dot_product = np.dot(s.T, w)
+    e = np.dot(dot_product, h)
+    attention_vector = softmax(e).dot(h.T).T
+    return attention_vector
+
+def additive_attention(decoder_hidden_state, encoder_hidden_states, v_add, W_add_enc, W_add_dec):
+    '''
+    decoder_hidden_state: np.array of shape (n_features_dec, 1)
+    encoder_hidden_states: np.array of shape (n_features_enc, n_states)
+    v_add: np.array of shape (n_features_int, 1)
+    W_add_enc: np.array of shape (n_features_int, n_features_enc)
+    W_add_dec: np.array of shape (n_features_int, n_features_dec)
+    
+    return: np.array of shape (n_features_enc, 1)
+        Final attention vector
+    '''
+    # your code here
+    s = decoder_hidden_state
+    h = encoder_hidden_states
+    e = (v_add.T).dot(np.tanh(np.dot(W_add_enc, h) + np.dot(W_add_dec, s)))
+    attention_vector = softmax(e).dot(h.T).T
+    return attention_vector
